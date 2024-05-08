e400347e56d8ea75a4aec8851bfc9f093a82a342
Kauê Lima <kaue.slima000@gmail.com>
2024-05-08 19:50:31 -0300

    firts

--- longevidade-web/src/components/home/index.tsx
+++ longevidade-web/src/components/home/index.tsx
@@ -6,7 +6,7 @@ import backgroundLast from "@/assets/home/arvore-verde.png"
 
 export default function Home() {
   return (
-    <section className="flex justify-center items-center   pt-10 flex-col w-full h-screen">
+    <section className="flex justify-center items-center pt-10 flex-col w-full h-screen">
       <div className="flex items-center px-10 justify-center w-full h-auto">
         <Image
           className="w-auto"
@@ -27,7 +27,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-center w-full">Protocolo Let's Go Forever 60 dias</h1>
         <p className="text-base text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
       </div>
-      <div className="h-1/5 flex px-10 items-start justify-center bg-second-color bg-no-repeat bg-cover w-full" style={{backgroundImage:`url(${backgroundLast.src})`}}>
+      <div className="h-[10%] flex px-10 items-start justify-center bg-second-color bg-no-repeat bg-cover w-full" style={{backgroundImage:`url(${backgroundLast.src})`}}>
             <button className="w-full flex items-center justify-center gap-2 h-10 bg-third-color rounded-lg text-second-color font-medium -mt-3" >
                 começar agora <FaArrowRightLong className="text-xl"/>
             </button>
